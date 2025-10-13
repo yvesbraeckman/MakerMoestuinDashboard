5f8e46ccc15bf9c9362b2c066a0987f99a88356a
Yves Braeckman <yves.braeckman.06@gmail.com>
2025-10-13 16:53:09 +0200

    added flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { CardComponent } from './card/card.component';
+import { DevicesComponent } from './devices/devices.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DevicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MakerFrontend';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
---
+++ src/app/devices/devices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-devices',
+  imports: [CardComponent],
+  templateUrl: './devices.component.html',
+  styleUrl: './devices.component.css'
+})
+export class DevicesComponent {
+
+}
